df95843f1c330adaafc027f6882576f73c1721ee
tford-dev <terranceford777@gmail.com>
2025-02-28 14:37:06 -0500

    seeing what's needed to change the database to postgres

--- models/index.js
+++ models/index.js
@@ -1,20 +1,28 @@
-
 'use strict';
-
+require('dotenv').config();
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const env = process.env.NODE_ENV || 'production';
+// const config = require(__dirname + '/../config/config.json')[env];
+const dbConfig = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.emailAddress, config.password, config);
-}
+// let sequelize;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.emailAddress, config.password, config);
+// }
+
+const sequelize = env === "production"
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: "postgres",
+      dialectOptions: { ssl: { require: true, rejectUnauthorized: false } }
+    })
+  : new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, dbConfig);
+
 
 fs
   .readdirSync(__dirname)
